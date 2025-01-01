a5f9c7a24c659bc56beb9abf55b6f3285818b1ec
Marcelo D'almeida <marcelodalmeida42@gmail.com>
2025-01-01 15:40:48 -0300

    fixes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app     = express();
+const path = require('path');
 var cors      = require('cors');
 app.use(cors());
 app.use(express.static('public'));
@@ -23,6 +24,18 @@ app.use('/hotsite', hotsiteRoute);
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/api/images/:filename', (req, res) => {
+  const filename = req.params.filename;
+  const filePath = path.join(__dirname, 'public', filename);
+
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      res.status(404).send('Imagem não encontrada');
+    }
+  });
+  
 });
 
 
